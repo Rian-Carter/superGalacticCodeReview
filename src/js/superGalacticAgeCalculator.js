210417cc0f0487c88133fa33cd4924830d576737
Rian Carter <R.Carter86@hotmail.com>
2022-03-11 15:36:40 -0800

    add code to pass test1

--- src/js/superGalacticAgeCalculator.js
+++ src/js/superGalacticAgeCalculator.js
@@ -1,11 +1,19 @@
 export default class GalacticAge {
-  constructor (earth) {
-    this.earth = earth;
-    this.mercury = this.earth / .24;
-    this.venus = this.earth / .62;
-    this.mars = this.earth / 1.88;
-    this.jupiter = this.earth / 11.86;
+  constructor () {
+    this.age = 20;
+    this.earthLifeExpectancy = 80;
+    this.mercury = 0.24;
+    this.venus = 0.62;
+    this.mars = 1.88;
+    this.jupiter = 11.86;
 }
 
-
+  // ageOnPlanet () {
+  //   this.earthLifeExpectancy = 80;
+  //   this.age = parseInt(this.earthLifeExpectancy / this.mercury);
+  //   this.age = this.earthLifeExpectancy / this.mercury;
+  //   this.age = this.earthLifeExpectancy / this.venus;
+  //   this.age = this.earthLifeExpectancy / this.mars;
+  //   this.age = this.earthLifeExpectancy / this.jupiter;
+  // }
 }
