58b63426f3ef5a79db6860c4855eaff9a8dc28a4
Rian Carter <R.Carter86@hotmail.com>
2022-03-11 11:18:44 -0800

    add src/, js/, superGalacticAgeCalculator.js, index.html, index.js, and object to pass first test

---
+++ src/js/superGalacticAgeCalculator.js
@@ -0,0 +1,10 @@
+export default class GalacticAge {
+  constructor () {
+    this.earth = 1;
+    this.mercury = .24;
+    this.venus = .62;
+    this.mars = 1.88;
+    this.jupiter = 11.86;
+    this.lifeExpectancy = 80;
+}
+}
