ec7663c817536d835b23469d8134fbb3ae300c68
Rian Carter <R.Carter86@hotmail.com>
2022-03-11 17:43:20 -0800

    fix error in math logic. tests and code actually calculate age on planet, not years remaining

--- __tests__/superGalacticAgeCalculator.tests.js
+++ __tests__/superGalacticAgeCalculator.tests.js
@@ -14,35 +14,35 @@ describe ('GalacticAge', () => {
 });
 
 describe ('calculateEarth', () => {
-  test('should return number of years remaining on Earth', () => {
+  test('should return years remaining on Earth', () => {
     let galacticAge = new GalacticAge(50);
     expect(galacticAge.calculateEarth()).toEqual(30);
   });
 });
 
 describe ('calculateMercury', () => {
-  test('should return number of years reamaining on Mercury', () => {
+  test('should return age on Mercury', () => {
     let galacticAge = new GalacticAge(50);
     expect(galacticAge.calculateMercury()).toEqual(208);
   });
 });
 
 describe ('calculateVenus', () => {
-  test('should return number of years remaining on Venus', () => {
+  test('should return age on Venus', () => {
     let galacticAge = new GalacticAge(50);
     expect(galacticAge.calculateVenus()).toEqual(81);
   });
 });
 
 describe ('calculateMars', () => {
-  test('should return number of years remaining on mars', () => {
+  test('should return age on mars', () => {
     let galacticAge = new GalacticAge(50);
     expect(galacticAge.calculateMars()).toEqual(27);
   });
 });
 
 describe ('calculateJupiter', () => {
-  test('should return number of years remaining on jupiter', () => {
+  test('should return age on jupiter', () => {
     let galacticAge = new GalacticAge(50);
     expect(galacticAge.calculateJupiter()).toEqual(4);
   });
