f98bdc7559b5da0f0c63af1eef74016db56e8a7b
Rian Carter <R.Carter86@hotmail.com>
2022-03-12 18:21:00 -0800

    refactor code

--- src/js/superGalacticAgeCalculator.js
+++ src/js/superGalacticAgeCalculator.js
@@ -8,43 +8,66 @@ export default class GalacticAge {
     this.jupiter = 11.86;
 };
 
-  earthYearsRemaining() {
-    return this.earthLifeExpectancy - this.age;
-  };
 
-  calculateAgeMercury() {
-    return Math.round(this.age/this.mercury); 
-  };
+// calculate age
+calculateAgeMercury() {
+  return (Math.round(this.age/this.mercury)); 
+};
 
-  calculateAgeVenus() {
-    return Math.round(this.age/this.venus);
-  };
+calculateAgeVenus() {
+  return (Math.round(this.age/this.venus));
+};
 
-  calculateAgeMars() {
-    return Math.round(this.age/this.mars);
-  };
+calculateAgeMars() {
+  return (Math.round(this.age/this.mars));
+};
 
-  calculateAgeJupiter() {
-    return Math.round(this.age/this.jupiter);
-  };
+calculateAgeJupiter() {
+  return (Math.round(this.age/this.jupiter));
+};
 
-  mercuryYearsRemaining() {
-    let mercuryYearsReamaining = this.calculateAgeMercury();
-    return mercuryYearsReamaining -= this.earthLifeExpectancy;
-  };
 
-  venusYearsRemaining() {
-    let venusYearsRemaining = this.calculateAgeVenus();
-    return venusYearsRemaining -= this.earthLifeExpectancy;
-  }
 
-  marsYearsRemaining() {
-    let marsYearsRemaining = this.calculateAgeMars();
-    return Math.abs(marsYearsRemaining -= this.earthLifeExpectancy);
-  }
 
-  jupiterYearsRemaining() {
-    let jupiterYearsRemaining = this.calculateAgeJupiter();
-    return Math.abs(jupiterYearsRemaining -= this.earthLifeExpectancy);
-  }
+//years left to live
+earthYearsRemaining() {
+  return this.earthLifeExpectancy - this.age;
+};
+
+mercuryYearsRemaining() {
+  let mercuryYearsReamaining = this.calculateAgeMercury();
+  return Math.abs(mercuryYearsReamaining -= this.earthLifeExpectancy);
+};
+
+venusYearsRemaining() {
+  let venusYearsRemaining = this.calculateAgeVenus();
+  return Math.abs(venusYearsRemaining -= this.earthLifeExpectancy);
+}
+
+marsYearsRemaining() {
+  let marsYearsRemaining = this.calculateAgeMars();
+  return Math.abs(marsYearsRemaining -= this.earthLifeExpectancy);
+}
+
+jupiterYearsRemaining() {
+  let jupiterYearsRemaining = this.calculateAgeJupiter();
+  return Math.abs(jupiterYearsRemaining -= this.earthLifeExpectancy);
+}
+
+
+
+
+// //refactoring code
+// agePlanets() {
+//   this.mercuryAge = parseFloat((this.age / this.mercury).toFixed(1));
+//   this.venusAge = (this.age / this.venus);
+//   this.marsAge = (this.age / this.mars);
+//   this.jupiterAge = (this.age / this.jupiter);
+// }
+// }
+// //life surpassed
+// lifeSurpassed() {
+//   if (this.age > this.earthLifeExpectancy) {
+//     this.mercurySurpassed = parseInt(((this.age - this.earthLifeExpectancy) / this.mercury).toFixed(1));
+//   }
 }
